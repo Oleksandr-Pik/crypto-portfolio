98c4f52e6754cad1f419ace78d989688b76b1d99
Oleksandr-Pik <pik.aleksandr79@gmail.com>
2024-03-17 19:56:24 +0200

    some fix

--- src/components/AddAssetForm.jsx
+++ src/components/AddAssetForm.jsx
@@ -13,11 +13,14 @@ import { useCrypto } from "../context/crypto-context";
 import CoinInfo from "./CoinInfo";
 
 const validateMessages = {
+  // eslint-disable-next-line
   required: "'${label}' is required!",
   types: {
+    // eslint-disable-next-line
     number: "${label} is not valid number",
   },
   number: {
+    // eslint-disable-next-line
     range: "${label} must be between ${min} and ${max}",
   },
 };
